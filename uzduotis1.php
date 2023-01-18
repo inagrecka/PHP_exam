bb6f5c0725db8bceba5368048ef67a58863a197a
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-18 20:11:32 +0200

    1 uzd sprendimas

--- uzduotis1.php
+++ uzduotis1.php
@@ -1,1 +1,25 @@
 <?php
+/*
+1.  Grąžinkite visų lyginių skaičių, esančių $numbers masyve, sumą (1 balas)
+*/
+   $numbers = [
+       15,
+       55,
+       66,
+       91,
+       100,
+       995,
+       17,
+       550,
+   ];
+
+function task1(array $num):int {
+    $sum = 0;
+    foreach ($num as $value) {
+        if ($value % 2 === 0) {
+            $sum += $value;
+        }
+    }
+    return $sum;
+}
+var_dump(task1($numbers));
