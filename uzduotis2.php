625f68a8e6f1b417e59bbdf979b1c885e1d176c3
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-20 10:01:58 +0200

    2 uzd galutinis sprendimas

--- uzduotis2.php
+++ uzduotis2.php
@@ -5,31 +5,14 @@
 $numbers = [
     [1, 3, 5],
     [55, 87, 100],
-    [12, 0, 0],
-
+    [12],
+    [],
 ];
-//echo $numbers[1][1];
-//die();
-
 function task2(array $arr):int {
-    for ($i = 0; $i < sizeof($arr,0); $i++) {
-        for ($j = 0; $j < sizeof($arr, 1); $j++) {
-//            if (!in_array($arr[$i],$arr) || !in_array($arr[$i][$j],$arr)) {
-//                $arr[$i] = 1;
-//                $arr[][$j] = 1;
-//            }
-            echo $arr[$i][$j];
-        }
+    $newArr = array();
+    foreach (array(0, 1, 2) as $a) {
+        $newArr[$a] = array_product(array_column($arr, $a));
     }
-//    $multi = 1;
-//    foreach ($arr as $item) {
-//        $multi *= $item;
-//        if ($item !== []) {
-//            $multi *= $item;
-//        } else {
-//            $item = 1;
-//        }
-//    }
-    return 0;
+    return array_product($newArr);
 }
 var_dump(task2($numbers));
