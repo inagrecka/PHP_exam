d93b639bb44575dd322640cd6412e3de683a5d68
LAPTOP-7RR447IQ\tatu8 <tatu89@gmail.com>
2023-01-20 13:41:27 +0200

    6 uzd salyga

---
+++ uzduotis6.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * 6. Parašykite programą, kuri per argumentų padavimą terminale paleidžiant funkciją juos priimtų
+ * ir juos sudaugintų tarpusavyje ir pakeltu kvadratu. Atkreipkite dėmesį, kad jeigu argumentas
+ * yra paduodamas ne skaičius, reikia, kad terminale gautumėme atitinkamą klaidos kodą ir pranešimą. (2 balai)
+ */
